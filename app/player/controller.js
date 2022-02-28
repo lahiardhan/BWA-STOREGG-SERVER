29235e9b755360d65626414a471878c505f64207
Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-02-28 19:04:24 +0700

    payment data added to detailPage endpoint

--- app/player/controller.js
+++ app/player/controller.js
@@ -30,11 +30,18 @@ module.exports = {
             .populate('nominals')
             .populate('user', '_id name phoneNumber')
 
+         const payment = await Payment.find().populate('banks')
+
          if(!voucher) {
             return res.status(404).json({ message: "voucher game tidak ditemukan.!" })
          }
    
-         res.status(200).json({ data: voucher })
+         res.status(200).json({ 
+            data: {
+               details: voucher,
+               payment
+            }
+          })
    
        } catch (err) {
          res.status(500).json({ message: err.message || `Internal server error` })
